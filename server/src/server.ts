e6e5c973219449f4cb2085c9302b1dd7e4625c18
Rafael <rafael.gcaldas01@gmail.com>
2023-01-16 23:07:38 -0300

    List habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello World!'
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
 app.listen({
