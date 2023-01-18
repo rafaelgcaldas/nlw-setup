335a387aa80d18ccb4e2d4d821c0fc5ec542f226
Rafael <rafael.gcaldas01@gmail.com>
2023-01-17 23:19:05 -0300

    Create habits - in progress

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,18 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
+
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
   
-    return habits
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+
+    const { title, weekDays } = createHabitBody.parse(request.body)
   })
 }
