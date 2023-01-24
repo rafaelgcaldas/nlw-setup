4f7cdfb73caefa0cc2b805760639ca46685d4200
Rafael <rafael.gcaldas01@gmail.com>
2023-01-24 00:06:16 -0300

    Header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,18 @@
-import { Habit } from "./components/Habit"
+// import { Habit } from "./components/Habit"
 
-function App() {
+import { Header } from "./components/Header"
+import "./styles/global.css"
+
+
+export function App() {
   return (
-  <Habit />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+        <Header/>
+
+      </div>
+    </div>
   )
 }
 
-export default App
